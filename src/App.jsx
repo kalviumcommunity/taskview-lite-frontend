02563806535455d3b75ca081276faf1b9e30ef12
varnikag24 <143873214+varnikag24@users.noreply.github.com>
2026-04-01 11:10:58 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+
+// TODO:
+// 1. Import BrowserRouter, Routes, Route
+// 2. Add routes:
+//    "/" → Home
+//    "/tasks" → Tasks
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* TODO: Add Routes here */}
+    </div>
+  );
+}
+
+export default App;
