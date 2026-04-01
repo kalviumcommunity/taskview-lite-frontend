a25e78d6221d31ba36949f61d0e524178145fd77
varnikag24 <143873214+varnikag24@users.noreply.github.com>
2026-04-01 11:10:35 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// TODO: Replace <a> with Link from react-router-dom
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="/">Home</a>
+      <a href="/tasks">Tasks</a>
+    </nav>
+  );
+}
+
+export default Navbar;
